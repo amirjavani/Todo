00fbb29ff192250b32463c4f4874b40241b9ba80
amirjavani <amirjavani@outlook.com>
2025-03-09 13:35:26 +0330

    footer dark theme

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,15 +1,20 @@
+import { useContext } from "react";
 import { IoLogoGithub, IoLogoLinkedin } from "react-icons/io";
 import { Link } from "react-router";
+import { ThemeContext } from "../Contexts/ThemeProvider";
 
 function Footer() {
+
+  const theme = useContext(ThemeContext);
+
   return (
-    <footer className="text-center border-t-blue-200 border-t-2 flex flex-row justify-between  p-5 mt-10 bg-blue-50 ">
+    <footer className={`text-center border-t-blue-200 border-t-2 flex flex-row justify-between  p-5 mt-10 ${theme?.theme=='dark'?'bg-[#4f5b75]':'bg-blue-50'} `}>
       <div className="flex flex-row justify-start items-center gap-1.5">
         <Link to={"https://github.com/amirjavani"}>
-          <IoLogoGithub className="text-3xl p-1 bg-blue-200 rounded-3xl" />
+          <IoLogoGithub className="text-3xl p-1 bg-blue-300 rounded-3xl" />
         </Link>
         <Link to={"https://www.linkedin.com/in/amirjavani/"}>
-          <IoLogoLinkedin className="text-3xl p-1 bg-blue-200 rounded-3xl" />
+          <IoLogoLinkedin className="text-3xl p-1 bg-blue-300 rounded-3xl" />
         </Link>
       </div>
 
