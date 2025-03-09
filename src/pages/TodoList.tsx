a7ed6cac54e3894dccea58b81053e57e3ea6e16d
amirjavani <amirjavani@outlook.com>
2025-03-09 16:54:21 +0330

    todos list fetch

--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -1,21 +1,48 @@
 import { MdAdd } from "react-icons/md";
 import Todo from "../components/Todo";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+interface todo{
+    id:number,
+    title:string
+}
 
 const TodoList = () => {
   const date = new Date();
-  
+
+  const [todosList, setTodosList] = useState<todo[]>();
+
+  const fetchTodoList = async () => {
+    try {
+      const res = await axios.get("http://localhost:5000/api/tasks");
+      setTodosList(res.data);
+    } catch (error) {
+      console.log("Errorr : " + error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTodoList();
+  }, []);
 
   return (
     <main className="border rounded ">
       <div className="flex flex-row  justify-between p-5 ">
         <h2 className="font-bold text-4xl ">Today </h2>
-        <span className="text-[20px] my-auto">{date.toISOString().split("T")[0]}</span>
+        <span className="text-[20px] my-auto">
+          {date.toISOString().split("T")[0]}
+        </span>
       </div>
       <div className="flex flex-col mx-10 p-2 my-2 divide-y divide-gray-400">
-        <Todo title="task1" />
-        <Todo title="task1" />
+        {todosList?.map((item) => (
+          <Todo title={item.title} />
+        ))}
       </div>
-      <button className="flex cursor-pointer flex-row mx-10 my-2 items-center p-2 rounded-3xl bg-green-600 hover:bg-green-700 active:text-[#232c40] text-lg "><MdAdd/>Add</button>
+      <button className="flex cursor-pointer flex-row mx-10 my-2 items-center p-2 rounded-3xl bg-green-600 hover:bg-green-700 active:text-[#232c40] text-lg ">
+        <MdAdd />
+        Add
+      </button>
     </main>
   );
 };
